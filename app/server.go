19a292bf2b4ebfdf71b656801f55f4f735e75703
Himank Jog <himankj@amazon.com>
2024-05-21 11:51:43 +0530

    feat: Added feature to listen to multiptle connection requests and respond with PONG

--- app/server.go
+++ app/server.go
@@ -15,19 +15,47 @@ func closeConnection(conn net.Conn) {
 }
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	defer closeConnection(conn)
+	conn, err := getPortConn()
 
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
+	buf := make([]byte, 1024)
+	for {
+		input, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading data: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Received: ", string(buf[:input]))
+		sendPongResponse(conn, err)
+	}
+}
+
+func getPortConn() (net.Conn, error) {
+	address := "0.0.0.0:6379"
+	listener, err := getPortListener(address)
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Failed to create connection on address ", address)
+		os.Exit(1)
+	}
+	defer closeConnection(conn)
+	return conn, err
+}
+
+func getPortListener(address string) (net.Listener, error) {
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Failed to bind to port on address ", address)
+		os.Exit(1)
+	}
+	return l, err
+}
+
+func sendPongResponse(conn net.Conn, err error) {
 	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
 		fmt.Println("Error writing data: ", err.Error())
