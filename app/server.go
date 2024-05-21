faaf9e6c645dbcf005d405a9b3f7bd2ee6cec04f
Himank Jog <himankj@amazon.com>
2024-05-21 11:09:32 +0530

    Feat: Returning PONG as a response when connecting to 6379 port

--- app/server.go
+++ app/server.go
@@ -6,15 +6,31 @@ import (
 	"os"
 )
 
+func closeConnection(conn net.Conn) {
+	err := conn.Close()
+	if err != nil {
+		fmt.Println("Error closing connection: ", err.Error())
+		os.Exit(1)
+	}
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	defer closeConnection(conn)
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing data: ", err.Error())
 		os.Exit(1)
 	}
 }
