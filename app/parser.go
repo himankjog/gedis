22ffbf3aa715a7f2ad81c5487c24f0dd34fa3024
Himank Jog <himankj@amazon.com>
2024-05-31 15:44:31 +0530

    feat: Added parser to decode integer, simple string, bulk string, arrays and errors

---
+++ app/parser.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"log"
+	"strconv"
+)
+
+type Type byte
+
+const (
+	INTEGER = ':'
+	STRING  = '+'
+	BULK    = '$'
+	ARRAY   = '*'
+	ERROR   = '-'
+)
+
+type RESP struct {
+	Type  Type
+	Data  []byte
+	Array []RESP
+}
+
+func Decode(data []byte) (RESP, error) {
+	if len(data) == 0 {
+		return RESP{}, errors.New("no data provided")
+	}
+	reader := bufio.NewReader(bytes.NewReader(data))
+	return decode(reader)
+}
+
+func decode(reader *bufio.Reader) (RESP, error) {
+	data, _, err := readNext(reader)
+	if err != nil {
+		log.Printf("Error trying to start decode: %s", err)
+		return RESP{}, err
+	}
+	switch data[0] {
+	case INTEGER:
+		return decodeInteger(data[1:])
+	case STRING:
+		return decodeSimpleString(data[1:])
+	case BULK:
+		bulkStringData, _, err := readNext(reader)
+		if err != nil {
+			log.Printf("Error trying to read bulk string data from decode: %s", err)
+			return RESP{}, err
+		}
+		bulkStringData = append(data, bulkStringData...)
+		return decodeBulkString(bulkStringData[1:])
+	case ERROR:
+		return decodeError(data[1:])
+	case ARRAY:
+		return decodeArray(data[1:], reader)
+	default:
+		log.Printf("Unsupported data type: %v", data[0])
+		return RESP{}, errors.New("unsupported data type")
+	}
+}
+
+func decodeInteger(data []byte) (RESP, error) {
+	reader := bufio.NewReader(bytes.NewReader(data))
+	integerData, charactersRead, err := readNext(reader)
+	if err != nil {
+		log.Printf("Error trying to read integer data in decodeInteger: %s", err)
+		return RESP{}, errors.New("error parsing integer data")
+	}
+	integer, err := strconv.Atoi(string(integerData[:charactersRead]))
+	if err != nil {
+		log.Printf("Error trying to parse integer from integer data: %s", err)
+		return RESP{}, errors.New("error parsing integer from integer data")
+	}
+	log.Printf("Parsed integer: %d", integer)
+	return RESP{
+		Type:  INTEGER,
+		Data:  integerData[:charactersRead],
+		Array: nil,
+	}, nil
+}
+
+func decodeSimpleString(data []byte) (RESP, error) {
+	reader := bufio.NewReader(bytes.NewReader(data))
+	simpleString, charactersRead, err := readNext(reader)
+	if err != nil {
+		log.Printf("Error trying to read simple string in decodeSimpleString: %s", err)
+		return RESP{}, err
+	}
+	log.Printf("Parsed simple string: %s", simpleString)
+	return RESP{
+		Type:  STRING,
+		Data:  simpleString[:charactersRead],
+		Array: nil,
+	}, nil
+}
+
+func decodeBulkString(data []byte) (RESP, error) {
+	bulkStringReader := bufio.NewReader(bytes.NewReader(data))
+	bulkStringLengthBytes, charactersRead, err := readNext(bulkStringReader)
+	if err != nil {
+		log.Printf("Error trying to read bulk string length bytes in decodeBulkString: %s", err)
+		return RESP{}, err
+	}
+	bulkStringLength, err := strconv.Atoi(string(bulkStringLengthBytes[:charactersRead]))
+	if err != nil {
+		log.Printf("Error trying to parse bulk string length from bulk string length bytes: %s", err)
+		return RESP{}, err
+	}
+	bulkStringBytes := make([]byte, bulkStringLength+2)
+	readStringLength, err := bulkStringReader.Read(bulkStringBytes)
+	if err != nil {
+		log.Printf("Error trying to read bulk string bytes in decodeBulkString: %s", err)
+		return RESP{}, err
+	}
+	if readStringLength != bulkStringLength+2 {
+		log.Printf("Error: read string length does not match bulk string length")
+		return RESP{}, errors.New("provided length does not match provided data length")
+	}
+
+	if readStringLength > 2 {
+		lastChar := bulkStringBytes[readStringLength-1]
+		secondToLastChar := bulkStringBytes[readStringLength-2]
+		if secondToLastChar != '\r' || lastChar != '\n' {
+			log.Printf("Error: bulk string does not end with CRLF")
+			return RESP{}, errors.New("bulk string does not end with CRLF")
+		}
+	}
+
+	return RESP{
+		Type:  BULK,
+		Data:  bulkStringBytes[:bulkStringLength],
+		Array: nil,
+	}, nil
+}
+
+func decodeError(data []byte) (RESP, error) {
+	reader := bufio.NewReader(bytes.NewReader(data))
+	errorData, charactersRead, err := readNext(reader)
+	if err != nil {
+		log.Printf("Error trying to read error data in decodeError: %s", err)
+		return RESP{}, err
+	}
+	log.Printf("Parsed error: %s", errorData)
+	return RESP{
+		Type:  ERROR,
+		Data:  errorData[:charactersRead],
+		Array: nil,
+	}, nil
+}
+
+func decodeArray(data []byte, reader *bufio.Reader) (RESP, error) {
+	arrayReader := bufio.NewReader(bytes.NewReader(data))
+	arrayLengthBytes, charactersRead, err := readNext(arrayReader)
+	if err != nil {
+		log.Printf("Error trying to read array length bytes in decodeArray: %s", err)
+		return RESP{}, err
+	}
+
+	arrayLength, err := strconv.Atoi(string(arrayLengthBytes[:charactersRead]))
+	if err != nil {
+		log.Printf("Error trying to parse array length from array length bytes: %s", err)
+		return RESP{}, err
+	}
+
+	var array []RESP
+	for i := 0; i < arrayLength; i++ {
+		element, err := decode(reader)
+		if err != nil {
+			log.Printf("Error trying to decode array element in decodeArray: %s", err)
+			return RESP{}, err
+		}
+		array = append(array, element)
+	}
+
+	return RESP{
+		Type:  ARRAY,
+		Data:  nil,
+		Array: array,
+	}, nil
+}
+
+func readNext(reader *bufio.Reader) ([]byte, int, error) {
+	line, err := reader.ReadBytes('\n')
+	if err != nil {
+		return nil, 0, err
+	}
+	if len(line) < 2 || line[len(line)-2] != '\r' {
+		return nil, 0, errors.New("carriage return character not found")
+	}
+	charactersRead := len(line) - 2
+	return line, charactersRead, nil
+}
---
+++ app/parser_test.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func validResponse(expectedResponse RESP, actualResponse RESP) bool {
+	if actualResponse.Type != expectedResponse.Type {
+		return false
+	}
+	if !bytes.Equal(actualResponse.Data, expectedResponse.Data) {
+		return false
+	}
+	if len(expectedResponse.Array) != len(actualResponse.Array) {
+		return false
+	}
+	arrayValidationStatus := true
+	for i, element := range expectedResponse.Array {
+		arrayValidationStatus = arrayValidationStatus && validResponse(element, actualResponse.Array[i])
+	}
+	return arrayValidationStatus
+}
+
+func TestDecodeInteger_ValidInteger(t *testing.T) {
+	input := []byte(":123\r\n")
+	expected := RESP{
+		Type:  INTEGER,
+		Data:  []byte("123"),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestDecodeInteger_ValidPositiveInteger(t *testing.T) {
+	input := []byte(":+123\r\n")
+	expected := RESP{
+		Type:  INTEGER,
+		Data:  []byte("+123"),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestDecodeInteger_NegativeInteger(t *testing.T) {
+	input := []byte(":-123\r\n")
+	expected := RESP{
+		Type:  INTEGER,
+		Data:  []byte("-123"),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestDecodeInteger_InvalidInteger_NoPrefixColon(t *testing.T) {
+	input := []byte("123\r\n")
+	expectedErr := "unsupported data type"
+	expectError(t, input, expectedErr)
+}
+
+func TestDecodeInteger_InvalidInteger_UnsupportedPrefix(t *testing.T) {
+	input := []byte("^123\r\n")
+	expectedErr := "unsupported data type"
+	expectError(t, input, expectedErr)
+}
+
+func TestDecodeInteger_InvalidInteger_MissingNextLineSuffix(t *testing.T) {
+	input := []byte(":123\r")
+	expectedErr := "EOF"
+	expectError(t, input, expectedErr)
+}
+
+func TestSimpleString_ValidSimpleString(t *testing.T) {
+	input := []byte("+ABC DEF\r\n")
+	expected := RESP{
+		Type:  STRING,
+		Data:  []byte("ABC DEF"),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestSimpleString_InvalidSimpleString_NoPrefixColon(t *testing.T) {
+	input := []byte("ABC\r\n")
+	expectedErr := "unsupported data type"
+	expectError(t, input, expectedErr)
+}
+
+func TestSimpleString_InvalidSimpleString_UnsupportedPrefix(t *testing.T) {
+	input := []byte("^ABC\r\n")
+	expectedErr := "unsupported data type"
+	expectError(t, input, expectedErr)
+}
+
+func TestSimpleString_InvalidSimpleString_MissingNextLineSuffix(t *testing.T) {
+	input := []byte("+ABC\r")
+	expectedErr := "EOF"
+	expectError(t, input, expectedErr)
+}
+
+func TestBulkString_ValidBulkString(t *testing.T) {
+	input := []byte("$5\r\nhello\r\n")
+	expected := RESP{
+		Type:  BULK,
+		Data:  []byte("hello"),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestBulkString_EmptyBulkString(t *testing.T) {
+	input := []byte("$0\r\n\r\n")
+	expected := RESP{
+		Type:  BULK,
+		Data:  []byte(""),
+		Array: nil,
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestBulkString_InvalidBulkString_InvalidLength(t *testing.T) {
+	input := []byte("$a\r\nhello\r\n")
+	expectedErr := "strconv.Atoi: parsing \"a\": invalid syntax"
+	expectError(t, input, expectedErr)
+}
+
+func TestBulkString_InvalidBulkStringLength_MissingNextLineSuffix(t *testing.T) {
+	input := []byte("$5\rhello\r\n")
+	expectedErr := "EOF"
+	expectError(t, input, expectedErr)
+}
+
+func TestBulkString_InvalidBulkStringLength_MoreThanDefinedData(t *testing.T) {
+	input := []byte("$5\r\nhelloWorld\r\n")
+	expectedErr := "bulk string does not end with CRLF"
+	expectError(t, input, expectedErr)
+}
+
+func TestBulkString_InvalidBulkStringLength_LessThanDefinedData(t *testing.T) {
+	input := []byte("$5\r\nhel\r\n")
+	expectedErr := "provided length does not match provided data length"
+	expectError(t, input, expectedErr)
+}
+
+func TestBulkString_ValidOneDimensionalArray(t *testing.T) {
+	input := []byte("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n")
+	expected := RESP{
+		Type: ARRAY,
+		Data: nil,
+		Array: []RESP{
+			{
+				Type:  BULK,
+				Data:  []byte("hello"),
+				Array: nil,
+			},
+			{
+				Type:  BULK,
+				Data:  []byte("world"),
+				Array: nil,
+			},
+		},
+	}
+
+	expectValidResponse(t, input, expected)
+}
+
+func TestBulkString_ValidNestedArray(t *testing.T) {
+	input := []byte("*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n")
+	expected := RESP{
+		Type: ARRAY,
+		Data: nil,
+		Array: []RESP{
+			{
+				Type: ARRAY,
+				Data: nil,
+				Array: []RESP{
+					{
+						Type:  INTEGER,
+						Data:  []byte("1"),
+						Array: nil,
+					},
+					{
+						Type:  INTEGER,
+						Data:  []byte("2"),
+						Array: nil,
+					},
+					{
+						Type:  INTEGER,
+						Data:  []byte("3"),
+						Array: nil,
+					},
+				},
+			},
+			{
+				Type: ARRAY,
+				Data: nil,
+				Array: []RESP{
+					{
+						Type:  STRING,
+						Data:  []byte("Hello"),
+						Array: nil,
+					},
+					{
+						Type:  ERROR,
+						Data:  []byte("World"),
+						Array: nil,
+					},
+				},
+			},
+		},
+	}
+	expectValidResponse(t, input, expected)
+}
+
+func TestBulkString_InvalidOneDimensionalArray(t *testing.T) {
+	input := []byte("*2\r$5\r\nhello\r\n$5\r\nworld\r\n")
+	expectedErr := "strconv.Atoi: parsing \"2\\r$5\": invalid syntax"
+	expectError(t, input, expectedErr)
+}
+
+func expectValidResponse(t *testing.T, input []byte, expected RESP) {
+	resp, err := Decode(input)
+	if err != nil {
+		t.Errorf("decodeInteger(%q) returned error %v", input, err)
+	}
+	if !validResponse(expected, resp) {
+		t.Errorf("decodeInteger(%q) = %v, want %v", input, resp, expected)
+	}
+}
+
+func expectError(t *testing.T, input []byte, expectedError string) {
+	expected := RESP{}
+
+	resp, err := Decode(input)
+	if err == nil {
+		t.Errorf("Decode(%q) = %v, want error", input, resp)
+	}
+	if err.Error() != expectedError {
+		t.Errorf("Decode(%q) returned error %v, want %v", input, err, expectedError)
+	}
+	if !validResponse(expected, resp) {
+		t.Errorf("Decode(%q) = %v, want %v", input, resp, expected)
+	}
+}
