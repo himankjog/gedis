e9e3427ea4722a6e052ee9ae79c5bd87f095c385
Himank Jog <himankj@amazon.com>
2024-05-31 17:25:35 +0530

    fix: Not getting EOF with new connection read, therefore manually breaking out of request read loop if data is less than buffer size

--- app/server.go
+++ app/server.go
@@ -100,7 +100,6 @@ func handleConnection(conn net.Conn, epollFd int, connFd int) {
 		readBuf := make([]byte, 1024)
 		bytesRead, err := reader.Read(readBuf)
 		log.Printf("Read %d bytes from %s", bytesRead, conn.RemoteAddr())
-		log.Printf("Data in readBuf: %q", readBuf)
 		if err != nil {
 			fmt.Printf("Error reading data from %s: %v", conn.RemoteAddr(), err.Error())
 			if err != io.EOF {
@@ -110,6 +109,10 @@ func handleConnection(conn net.Conn, epollFd int, connFd int) {
 			break
 		}
 		requestData = append(requestData, readBuf[:bytesRead]...)
+		if bytesRead < 1024 {
+			// If we don't have anymore data to read, we can safely quit the read loop
+			break
+		}
 	}
 	log.Printf("Received data from %s: %q", conn.RemoteAddr(), requestData)
 	sendPongResponse(conn)
